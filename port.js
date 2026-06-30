94a302afda2fe1325d3e3de34760ee61fffa4a12
Faruq-srx <faruqolamide142@gmail.comaruq@olamide:~$ git config --global user.name Faruq-srxfaruq@olamide:~$ git config --global user.email faruqolamide142@gmail.com>
2026-06-30 18:43:13 +0100

    my portfolio first stage done

--- port.js
+++ port.js
@@ -5,11 +5,14 @@ const allProjects = document.querySelectorAll('.project1');
 projects.style.display = 'none';
 const aboutPage=document.querySelector('.about')
 aboutPage.style.display='none';
+const contactPage=document.querySelector('.contact-page');
+contactPage.style.display='none';
 
 function viewProject() {
   hero.style.display = 'none';
   projects.style.display = 'flex';
   aboutPage.style.display='none';
+   contactPage.style.display='none';
 }
 viewProjectButton.addEventListener('click', viewProject);
 
@@ -18,7 +21,7 @@ const nextButton= document.querySelector('.right-arrow');
 console.log(nextButton)
 const prevButton=document.querySelector('.left-arrow');
 
-let currentIndex=0
+let currentIndex= 0;
 function updatePage() {
   allProjects.forEach((project, index) => {
     if(index===currentIndex) {
@@ -30,20 +33,22 @@ function updatePage() {
   });
 };
 
-
+const firstNum=document.querySelector('.first');
 nextButton.addEventListener('click',()=>{
   if(currentIndex < allProjects.length-1) {
     currentIndex++;
+    firstNum.innerHTML=`${currentIndex + 1}`;
     updatePage();
   }
 });
 
 prevButton.addEventListener('click',()=> {
   if(currentIndex > 0) {
+    firstNum.innerHTML=`${currentIndex}`;
     currentIndex--;
     updatePage();
   }
-})
+});
 
 //About page
 const aboutButton=document.querySelector('.aboutt')
@@ -51,6 +56,7 @@ function showAboutpage() {
   hero.style.display='none';
   aboutPage.style.display='block';
   projects.style.display='none';
+   contactPage.style.display='none';
 }
 aboutButton.addEventListener('click',showAboutpage);
 
@@ -59,7 +65,21 @@ const homeButton=document.querySelector('.home');
 function showHomepage() {
   hero.style.display='flex';
   projects.style.display='none';
-  aboutPage.style.display='none'
+  aboutPage.style.display='none';
+   contactPage.style.display='none';
 }
 
-homeButton.addEventListener('click',showHomepage)
+homeButton.addEventListener('click',showHomepage);
+
+//contact page
+const getInTouch=document.querySelector('.btn');
+const contactBtn=document.querySelector('.contact');
+function contactPagedisplay() {
+  contactPage.style.display='block';
+  hero.style.display='none';
+  projects.style.display='none';
+  aboutPage.style.display='none';
+}
+getInTouch.addEventListener('click',contactPagedisplay);
+contactBtn.addEventListener('click',contactPagedisplay)
+ 
